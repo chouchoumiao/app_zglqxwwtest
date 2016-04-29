887a1fe0de269547acab9d7ace4226e5ad1d5d2c
wu_jy1984@126.com <84112326Wu>
2016-04-29 10:41:46 +0800

    win追加commonController

---
+++ libs/Controller/commonController.class.php
@@ -0,0 +1,21 @@
+<?php
+class commonController{
+	protected function delInfoByID($model,$method){
+		if(!isset($_POST["id"])){
+			$arr['success'] = 0;
+			$arr['msg'] = '无法取得id！';
+			return $arr;
+		}
+		$id = intval(addslashes($_POST["id"]));
+
+		if(M($model)->$method($id)){
+			$arr['success'] = 1;
+			$arr['msg'] = '删除成功！';
+		}else{
+			$arr['success'] = 0;
+			$arr['msg'] = '删除失败';
+		}
+		return $arr;
+
+	}
+}
